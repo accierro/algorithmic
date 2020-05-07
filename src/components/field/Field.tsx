b1c01da6473cff10cbeeb2c1dd07e63e0218965b
Vlad <accierro@gmail.com>
2020-05-07 09:39:20 +0100

    Rough implementation
    Implement visualizer of breadth-first solution

--- src/components/field/Field.tsx
+++ src/components/field/Field.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useMachine } from "@xstate/react";
 import "../../css/field.scss";
-import FieldEngine from "../../class/FieldEngine";
 import FieldStateMachine from "../../machine/FieldStateMachine";
 import _ from "lodash";
 
@@ -32,88 +31,106 @@ function getGrid(rows: number, columns: number): Cell[][] {
 }
 
 const Field: React.FC<{}> = () => {
+  const [counter, setCounter] = useState(0);
+  const ref = useRef<{ copy: Cell[][]; queue: Cell[]; target: Cell } | null>(
+    null
+  );
   const [grid, setGrid] = useState<Cell[][]>(getGrid(30, 70));
   const [start, setStart] = useState<{ x: number; y: number } | null>(null);
   const [end, setEnd] = useState<{ x: number; y: number } | null>(null);
   const [state, send] = useMachine(FieldStateMachine);
-  //   const [counter, setState] = useState(0);
   const requestRef = useRef<number | null>(null);
-  console.log(grid);
   useEffect(() => {
     if (start && end) {
-      const copy = _.cloneDeep(grid);
-      copy[start.x][start.y].visited = true;
-      const queue = [copy[start.x][start.y]];
-      const target = copy[end.x][end.y];
-
-      while (queue.length !== 0) {
-        const node = queue.shift() as Cell;
-
-        const { x, y } = node;
-        if (x + 1 < 30) {
-          const check = copy[x + 1][y];
-          if (check === target) {
-            console.log("FOUND");
-            break;
-          } else if (!check.visited) {
-            check.visited = true;
-            queue.push(check);
-          }
-        }
-
-        if (y + 1 < 70) {
-          const check = copy[x][y + 1];
-          if (check === target) {
-            console.log("FOUND");
-            break;
-          } else if (!check.visited) {
-            check.visited = true;
-            queue.push(check);
-          }
-        }
-        if (x - 1 >= 0) {
-          const check = copy[x - 1][y];
-          if (check === target) {
-            console.log("FOUND");
-            break;
-          } else if (!check.visited) {
-            check.visited = true;
-            queue.push(check);
-          }
-        }
-
-        if (y - 1 >= 0) {
-          const check = copy[x][y - 1];
-          if (check === target) {
-            console.log("FOUND");
-            break;
-          } else if (!check.visited) {
-            check.visited = true;
-            queue.push(check);
-          }
-        }
+      if (ref.current === null) {
+        const copy = _.cloneDeep(grid);
+        copy[start.x][start.y].visited = true;
+        const queue = [copy[start.x][start.y]];
+        const target = copy[end.x][end.y];
+        ref.current = {
+          copy,
+          queue,
+          target,
+        };
+        setCounter((prev) => prev + 1);
       }
-
-      setGrid(copy);
     }
   }, [start, end]);
 
-  console.log(state);
-  //   const animate = () => {
-  //     setState((prev) => prev + 1);
-  //     requestRef.current = requestAnimationFrame(animate);
-  //   };
+  const animate = () => {
+    const { queue, copy, target } = ref.current as {
+      copy: Cell[][];
+      queue: Cell[];
+      target: Cell;
+    };
+    let found = false;
+    if (queue.length !== 0) {
+      const node = queue.shift() as Cell;
 
-  //   useEffect(() => {
-  //     requestRef.current = requestAnimationFrame(animate);
-  //     return () =>
-  //       requestRef.current !== null
-  //         ? cancelAnimationFrame(requestRef.current)
-  //         : undefined;
-  //   }, []);
+      const { x, y } = node;
+      if (x + 1 < 30) {
+        const check = copy[x + 1][y];
+        if (check === target) {
+          console.log("FOUND");
+          found = true;
+        } else if (!check.visited) {
+          check.visited = true;
+          queue.push(check);
+        }
+      }
+
+      if (y + 1 < 70) {
+        const check = copy[x][y + 1];
+        if (check === target) {
+          console.log("FOUND");
+          found = true;
+        } else if (!check.visited) {
+          check.visited = true;
+          queue.push(check);
+        }
+      }
+      if (x - 1 >= 0) {
+        const check = copy[x - 1][y];
+        if (check === target) {
+          console.log("FOUND");
+          found = true;
+        } else if (!check.visited) {
+          check.visited = true;
+          queue.push(check);
+        }
+      }
+
+      if (y - 1 >= 0) {
+        const check = copy[x][y - 1];
+        if (check === target) {
+          console.log("FOUND");
+          found = true;
+        } else if (!check.visited) {
+          check.visited = true;
+          queue.push(check);
+        }
+      }
+    }
+
+    setGrid(copy);
+
+    if (!found) {
+      setCounter((prev) => prev + 1);
+      requestRef.current = requestAnimationFrame(animate);
+    }
+  };
+
+  useEffect(() => {
+    if (ref.current) {
+      requestRef.current = requestAnimationFrame(animate);
+    }
+    return () =>
+      requestRef.current !== null
+        ? cancelAnimationFrame(requestRef.current)
+        : undefined;
+  }, [ref.current]);
   const rows = 30;
   const columns = 70;
-  console.time("grid");
   const arr = grid.map((r) => (
     <tr>
       {r.map((c) => {
@@ -144,7 +161,6 @@ const Field: React.FC<{}> = () => {
                 });
               }
               if (end === null && start !== null) {
-                console.log("SELECTING END");
                 setEnd({ x: c.x, y: c.y });
                 setGrid((prev) => {
                   return prev.map((i) => {
@@ -165,12 +181,14 @@ const Field: React.FC<{}> = () => {
       })}
     </tr>
   ));
-  console.timeEnd("grid");
 
   return (
-    <table className="field">
-      <tbody>{arr}</tbody>
-    </table>
+    <>
+      <table className="field">
+        <tbody>{arr}</tbody>
+      </table>
+      <button onClick={() => setCounter((prev) => prev + 1)}>Rerender</button>
+    </>
   );
 };
 
